c057ba3d528928a27e55aa888480327bbb8dadae
musk <musk.chan.c@gmail.com>
2020-03-10 11:34:51 +0800

    stringstreams

--- tutorial.cpp
+++ tutorial.cpp
@@ -1,31 +1,21 @@
-// assignment operator
-//
-
+// stringstreams
 #include <iostream>
+#include <string>
+#include <sstream>
 using namespace std;
 
 int main()
 {
-    int a, b;
-    a = 10;
-    b = 4;
-    a = b;
-    b = 7;
-    a = 111;
+    string mystr;
+    float price = 0;
+    int quantity = 0;
 
-    cout << "a: ";
-    cout << a;
-    cout << "   b: ";
-    cout << b;
+    cout << "Enter price: ";
+    getline(cin, mystr);
+    stringstream(mystr) >> price;
+    cout << "Enter quantity: ";
+    getline(cin, mystr);
+    stringstream(mystr) >> quantity;
+    cout << "Total price: " << price * quantity << endl;
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
