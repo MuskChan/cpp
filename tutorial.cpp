49adc1c5b8c42dc0d9af1cbd5ff41b427940b347
musk <musk.chan.c@gmail.com>
2020-04-07 01:25:35 +0800

    Dynamic memory

--- tutorial.cpp
+++ tutorial.cpp
@@ -1,21 +1,11 @@
-// stringstreams
+// Dynamic memory
 #include <iostream>
-#include <string>
-#include <sstream>
 using namespace std;
 
 int main()
 {
-    string mystr;
-    float price = 0;
-    int quantity = 0;
-
-    cout << "Enter price: ";
-    getline(cin, mystr);
-    stringstream(mystr) >> price;
-    cout << "Enter quantity: ";
-    getline(cin, mystr);
-    stringstream(mystr) >> quantity;
-    cout << "Total price: " << price * quantity << endl;
-    return 0;
+	int* foo;
+	foo = new int[5];
+	cout << foo;
+	return 0;
 }
