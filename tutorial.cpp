8ae54467ed39375595a65a19121ef63df21bd1ff
musk <musk.chan.c@gmail.com>
2020-04-07 12:25:55 +0800

    structures

--- tutorial.cpp
+++ tutorial.cpp
@@ -1,13 +1,38 @@
-// rememb-o-matic
+// array of structures
 #include <iostream>
-#include <new>
+#include <string>
+#include <sstream>
 using namespace std;
+
+struct movies_t {
+    string title;
+    int year;
+} films[3];
+
+void printmovie(movies_t movie);
 
 int main()
 {
-    int i, n;
-    int* p;
-    cout << "How many numbers would you like to type? ";
-    cin >> i;
+    string mystr;
+    int n;
+
+    for (n = 0; n < 3; n++)
+    {
+        cout << "Enter title: ";
+        getline(cin, films[n].title);
+        cout << "Enter year: ";
+        getline(cin, mystr);
+        stringstream(mystr) >> films[n].year;
+    }
+
+    cout << "\nYou have entered these movies:\n";
+    for (n = 0; n < 3; n++)
+        printmovie(films[n]);
     return 0;
+}
+
+void printmovie(movies_t movie)
+{
+    cout << movie.title;
+    cout << " (" << movie.year << ")\n";
 }
